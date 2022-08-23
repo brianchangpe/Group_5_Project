7e2a23fed2103c1531ed37bb00a114e675353c36
prakrutipg <prakrutipg@gmail.com>
2022-08-22 20:09:19 -0400

    Adding streamlit draft

---
+++ Part-3/Poll.py
@@ -0,0 +1,18 @@
+# Import the libraries
+import streamlit as st
+import pandas as pd
+
+# Create a title for your application using markdown syntax and Streamlit
+st.title("Create a New Poll")
+
+# Create a subheader for your application
+st.subheader("You can only cast one vote")
+
+# Bonus
+library = st.selectbox(
+    "Please select below",
+    ("Yes", "No")
+)
+
+if st.button("Display selection"):
+    st.write(library)
