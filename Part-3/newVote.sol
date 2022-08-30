1eab717a807e0ba8defea0adcce6e087251d2c82
brianchangpe <brianchang.pe@gmail.com>
2022-08-29 22:01:51 -0400

    final project

--- Part-3/newVote.sol
+++ Part-3/newVote.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.5.0;
-
-
-import "./complexCityToken.sol";
+//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
+//import "./complexCityToken.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
-contract ComVote is ERC20{
 
+contract ComVote is ERC20, ERC20Mintable{
     struct Poll{
         string name;
         string uri;
@@ -13,16 +13,18 @@ contract ComVote is ERC20{
         uint voteSupport;   
         uint pollId; 
         uint pollDeadlineInHours;
-    }
+        }
 
     address payable public chairperson;
     mapping(uint => Poll) public polls;
+    //mapping(uint => address[] ) pollVoters; 
     mapping (uint => mapping(address => bool)) public pollVoters;
     mapping(uint => mapping(address => bool)) public pollCommittee;
     uint public NumberOfPolls = 0;
 
     constructor()public{
         chairperson=msg.sender;
+        //addMinter(chairperson);
     }
 
     modifier isCommittee(uint pollId){
@@ -37,20 +39,26 @@ contract ComVote is ERC20{
         _;
     }
     
+    modifier isChairperson{
+        require (msg.sender == chairperson);
+        _;
+    }
+
     modifier isActive (uint pollId){
         require(polls[pollId].pollDeadlineInHours > now, "This poll has ended.");
         _;
     }
 
-    function getBalance(address newVoter) public view returns(uint ){
-    return newVoter.balance;
-}
-
     function addVoter(uint pollId, address newVoter) public isCommittee(pollId) {
         pollVoters[pollId][newVoter] = true;
-
     }
 
+    function addVotertoAll(address newVoter) public isChairperson{
+        for (uint i=1; i <= NumberOfPolls; i++){
+            pollVoters[i][newVoter] = true;
+        }
+    }
+    
     function vote(uint pollId, bool support) public isVoter(pollId){
         polls[pollId].voteCount += 1;
         if (support){
@@ -63,6 +71,8 @@ contract ComVote is ERC20{
     function hasMajority(uint pollId) public view returns(bool){
         return ( (polls[pollId].voteCount) < polls[pollId].voteSupport * 2);
     }
+
+
 
     function createPoll(string memory _name, string memory _uri, uint pollDeadlineInHours) public returns(uint256) {
         require (msg.sender == chairperson);
